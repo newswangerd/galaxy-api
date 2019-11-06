199f7830f3181bdf717f9d53b2d138d2a44c09a3
David Newswanger <dnewswan@redhat.com>
2019-11-06 08:55:52 -0500

    fix tests

--- tests/api/test_api_ui_collection_viewsets.py
+++ tests/api/test_api_ui_collection_viewsets.py
@@ -14,3 +14,4 @@ class TestCollectionVersionViewSet(BaseTestCase):
         patcher = mock.patch("galaxy_pulp.GalaxyCollectionVersionsApi", spec=True)
         self.versions_api = patcher.start().return_value
         self.addCleanup(patcher.stop)
+        
